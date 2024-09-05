d30afa5cbf18fc4ebbfb72ba520d4909a8833e8b
ksio2124 <ksio2124@protonmail.ch>
2024-09-04 21:38:34 -0400

    updated package version and add .env

--- trivia-app/front-end-react/src/config.js
+++ trivia-app/front-end-react/src/config.js
@@ -1,3 +1,4 @@
+console.log(process.env.REACT_APP_WEBSOCKET_ENDPOINT)
 module.exports = {
-  WebsocketEndpoint: 'REPLACE_WITH_WWS_ENDPOINT'
+  WebsocketEndpoint: process.env.REACT_APP_WEBSOCKET_ENDPOINT
 };
--- trivia-app/front-end-react/src/index.js
+++ trivia-app/front-end-react/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'dotenv/config';
 
+console.log(process.env.WEBSOCKET_ENDPOINT)
 ReactDOM.render(
   <React.StrictMode>
     <App />
